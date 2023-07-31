516341bdd446fae85be6485d9b007ec2927bc3ef
ismaelhugo <ismael.oliveira@somagrupo.com.br>
2023-07-30 23:02:55 -0300

    Save to file

--- getTweets.py
+++ getTweets.py
@@ -5,12 +5,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-s=Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
+driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get('https://twitter.com/')
 time.sleep(2)
-driver.find_element(By.CLASS_NAME, 'css-4rbku5').click()
+driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a').click()
 time.sleep(2)
 driver.find_element(By.NAME, 'text').send_keys("IsmaelHugoDev")
 driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div').click()
@@ -20,21 +19,53 @@ driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[
 time.sleep(5)
 
 searchElement = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div/label/div[2]/div/input')
-searchElement.send_keys("coleção farm until:2023-06-07 since:2023-06-01")
+searchElement.send_keys("coleção são joao farm")
 searchElement.send_keys(Keys.RETURN)
 
 time.sleep(3)
 
 Tweets=[]
 
+no_of_pagedowns = 20
+elem = driver.find_element(By.XPATH, "html/body")
+
+while no_of_pagedowns:
+    elem.send_keys(Keys.PAGE_DOWN)
+    time.sleep(1)
+    no_of_pagedowns-=1
+
+time.sleep(5)
+
 articles = driver.find_elements(By.XPATH,"//article[@data-testid='tweet']")
 
-time.sleep(1)
+time.sleep(20)
+Tweets = []
 
-while True:
-    for article in articles:
-        Tweet = driver.find_element(By.XPATH,".//div[@data-testid='tweetText']").get_attribute("innerHTML")
-        time.sleep(3)
-        print(Tweet)
+for article in articles:
+    Tweets.append(article.text)
+    print("OK")
 
-time.sleep(30)
+time.sleep(10)
+
+def salvar_tweets_em_arquivo(tweets, nome_arquivo):
+    try:
+        # Verificar se o arquivo já existe e adicioná-los ao final
+        with open(nome_arquivo, 'a') as arquivo:
+            for tweet in tweets:
+                arquivo.write(tweet + "\n")  # Adicionar cada tweet ao arquivo
+        print("Tweets salvos com sucesso no arquivo.")
+    except FileNotFoundError:
+        # Se o arquivo não existir, criar um novo e salvar os tweets
+        with open(nome_arquivo, 'w') as arquivo:
+            for tweet in tweets:
+                arquivo.write(tweet + "\n")  # Adicionar cada tweet ao arquivo
+        print("Arquivo criado e tweets salvos com sucesso.")
+
+nome_arquivo = "tweets.txt"
+
+time.sleep(10)
+
+# Chamando a função para salvar os tweets no arquivo
+salvar_tweets_em_arquivo(Tweets, nome_arquivo)
+
+time.sleep(10)
